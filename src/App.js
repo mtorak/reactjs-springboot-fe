d9b88be3caa2d823fcafb0a09e07bc0a17c2e69a
john doe <johndoe@acme.com>
2024-10-18 17:05:46 +0300

    fetch be /client data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import logo from './logo.svg'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+    state = {
+        clients: []
+    };
 
+    async componentDidMount() {
+        const response = await fetch('/client');
+        const body = await response.json();
+        this.setState({ clients: body });
+        console.log("Body: " + body);
+    }
+
+    render() {
+        // const { clients } = this.state;
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <div className="App-intro">
+                        <h2>Clients</h2>
+                        {this.state.clients.map(client =>
+                            <div key={client.id}>
+                                {client.name} ({client.email})
+                            </div>
+                        )}
+                    </div>
+                </header>
+            </div>
+        );
+    }
+}
 export default App;
