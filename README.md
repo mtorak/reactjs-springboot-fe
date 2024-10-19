47be5ac0cc8e0ac1e713a19f44c7f5eea5431e05
john doe <johndoe@acme.com>
2024-10-19 15:25:21 +0300

    new components

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+3.5. Creating a ClientEdit Component
+
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,35 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ClientList from './ClientList';
+import ClientEdit from './ClientEdit';
+import ClientNew from './ClientNew';
+import Home from './Home';
 import './App.css';
 
 class App extends Component {
-    state = {
-        clients: []
-    };
-
-    async componentDidMount() {
-        const response = await fetch('/client');
-        const body = await response.json();
-        this.setState({ clients: body });
-        console.log("Body: " + body);
-    }
 
     render() {
-        // const { clients } = this.state;
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <div className="App-intro">
-                        <h2>Clients</h2>
-                        {this.state.clients.map(client =>
-                            <div key={client.id}>
-                                {client.name} ({client.email})
-                            </div>
-                        )}
-                    </div>
-                </header>
-            </div>
+
+            <Router>
+                <Switch>
+                    <Route path='/' exact={true} component={Home} />
+                    <Route path='/client' exact={true} component={ClientList} />
+                    <Route path='/client/:id' component={ClientEdit} />
+                </Switch>
+            </Router>
+
+            // <Router>
+            //     <Switch>
+            //         {/*Home page */}
+            //         <Route exact path="/home" component={Home} />
+
+            //         {/* Route for client list */}
+            //         <Route exact path="/" component={ClientList} />
+
+            //         {/* Route for client edit */}
+            //         <Route path="/client/:id" component={ClientEdit} />
+
+            //         {/* Route for new client */}
+            //         <Route path="/client/new" component={ClientNew} />
+
+            //         {/* Optionally, add a redirect or a default route */}
+            //     </Switch>
+            // </Router>
+
         );
     }
 }
